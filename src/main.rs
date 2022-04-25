76d9ae4293ad8bbfa08da3b27886a929c03f3a05
phxtho <phathopukwana@gmail.com>
2022-04-25 21:40:17 +0200

    set up a request to the open ai api

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
 use dotenv::dotenv;
 use std::env;
 use reqwest;
-use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>  {
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY");
-    println!("{api_key}");
-    let command = env::args().nth(1).expect("no command provided");
-    println!("{command}");
+    let prompt = env::args().nth(1).expect("no prompt provided");
 
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
+    let client = reqwest::Client::new();
+
+    let res = client.post("https://api.openai.com/v1/engines/text-davinci-002/completions")
+        .header("Content-Type","application/json")    
+        .header("Authorization", format!("Bearer {}", api_key))  
+        .json(&serde_json::json!({
+            "prompt": prompt,
+            "max_tokens": 5,
+            "temperature": 1,
+            "top_p": 1,
+            "n": 1,
+            "stream": false,
+            "logprobs": null,
+            "stop": "\n"
+        }))
+        .send()
         .await?;
-     println!("{:#?}", resp);
+
+     println!("{:#?}", res);
     Ok(())
 }
