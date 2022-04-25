a1e454dc0780291a5197fdafb86d3a26129b78fd
phxtho <phathopukwana@gmail.com>
2022-04-25 20:48:37 +0200

    set up async runtime (tokio) and http client (reqwest)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use dotenv::dotenv;
 use std::env;
+use reqwest;
+use std::collections::HashMap;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>>  {
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY");
     println!("{api_key}");
     let command = env::args().nth(1).expect("no command provided");
     println!("{command}");
+
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+     println!("{:#?}", resp);
+    Ok(())
 }
