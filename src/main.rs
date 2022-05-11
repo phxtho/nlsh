40190f69015f7caf66781f29cdf222f963327124
phxtho <phathopukwana@gmail.com>
2022-05-11 13:54:33 +0200

    adjust prompt & deserialize the response body

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 use dotenv::dotenv;
 use std::env;
 use reqwest;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct CompletionResponse {
+    pub choices: Vec<CompletionChoices>
+}
+
+#[derive(Deserialize)]
+struct CompletionChoices {
+    pub text: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>>  {
+    // Retreive API Key from env vars
     dotenv().ok();
     let api_key = env::var("API_KEY").expect("API_KEY");
-    let prompt = env::args().nth(1).expect("no prompt provided");
+
+    // Parse command argument
+    let user_prompt : String = env::args().nth(1).expect("no prompt provided");
 
     let client = reqwest::Client::new();
 
-    let res = client.post("https://api.openai.com/v1/engines/text-davinci-002/completions")
+    let prompt: String = format!("Input: List files\nOutput: ls -l\nInput: {user_prompt}\nOutput:");
+
+    let res = client.post("https://api.openai.com/v1/engines/code-davinci-002/completions")
         .header("Content-Type","application/json")    
         .header("Authorization", format!("Bearer {}", api_key))  
         .json(&serde_json::json!({
             "prompt": prompt,
-            "max_tokens": 5,
-            "temperature": 1,
+            "max_tokens": 100,
+            "temperature": 0,
             "top_p": 1,
             "n": 1,
             "stream": false,
@@ -25,7 +41,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>>  {
         }))
         .send()
         .await?;
-
-     println!("{:#?}", res);
+    
+    if res.status() == 200 {
+        let body = res.json::<CompletionResponse>().await?;
+        println!("{:#?}", body.choices[0].text );
+    }
+     
     Ok(())
 }
